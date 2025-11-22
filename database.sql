2018f2e5017e6c987cb6149d372dbe86e503c570
Julian Bejarano <julianbejarano314@gmail.com>
2025-11-22 10:45:50 -0500

    Base de datos 22/11/2025

--- database.sql
+++ database.sql
@@ -2,6 +2,23 @@ CREATE DATABASE Facturacion;
 
 USE Facturacion;
 
+ CREATE TABLE t_rol(                                                                                                                    
+    id_rol          int(11)         AUTO_INCREMENT     NOT NULL,
+    t_rol           varchar(100)                       NOT NULL,
+    CONSTRAINT      pk_rol      PRIMARY KEY(id_rol)
+ )ENGINE=InnoDb;
+
+ CREATE TABLE t_usuario(
+    id_usuario      int(11)         AUTO_INCREMENT      NOT NULL,
+    nombre          varchar(100)        NOT NULL,
+    apellido        varchar(100)        NOT NULL,
+    correo          varchar(100)        NOT NULL,
+    password        varchar(100)        NOT NULL,
+    id_rol          int(11)             NOT NULL,
+    CONSTRAINT      pk_usuario          PRIMARY KEY(id_usuario),
+    CONSTRAINT      fk_rol              FOREIGN KEY(id_rol) REFERENCES  t_rol(id_rol)
+ )ENGINE=InnoDb;
+
 CREATE TABLE t_ciudad(
     id_ciudad       int(11)         AUTO_INCREMENT      NOT NULL,
     ciudad          varchar(100),
@@ -42,25 +59,34 @@ CREATE TABLE t_cliente(
 
  CREATE TABLE t_factura(
     id_factura      int(11)     AUTO_INCREMENT      NOT NULL,
-    fecha           date,
+    id_usuario      int(11),    
     id_cliente      int(11),
     id_producto     int(11),
+    fecha           date,
     cantidad        varchar(50),
     valor_unitario  float(10,2),
     CONSTRAINT      pk_factura   PRIMARY KEY(id_factura),
+    CONSTRAINT      fk_usuario      FOREIGN KEY(id_usuario) REFERENCES t_usuario(id_usuario),
     CONSTRAINT      fk_cliente      FOREIGN KEY(id_cliente) REFERENCES t_cliente(id_cliente),
     CONSTRAINT      fk_producto       FOREIGN KEY(id_producto) REFERENCES t_producto(id_producto)
  )ENGINE=InnoDb;
 
 
-//SELECT fac.id_factura,
-		fac.fecha,
-        fac.id_cliente,
-        cli.nombre_cliente,
-        cli.direccion,
-        fac.id_producto,
-        fac.cantidad,
-        fac.valor_unitario
-FROM t_factura	fac
-INNER JOIN t_cliente cli ON cli.id_cliente	= cli.id_cliente,
-INNER JOIN t_producto pro ON pro.id_producto	= pro.id_producto//
+--Factura
+SELECT	FAC.id_factura,
+		FAC.fecha,
+		USU.nombre,
+        USU.apellido,
+        FAC.id_cliente,
+        CLI.nombre_cliente,
+        CLI.direccion,
+        FAC.id_producto,
+        PRO.producto,
+        FAC.cantidad,
+        FAC.valor_unitario,
+        FAC.cantidad * FAC.valor_unitario AS Total
+        
+FROM t_factura FAC
+INNER JOIN t_usuario USU ON USU.id_usuario = FAC.id_usuario
+INNER JOIN t_cliente CLI ON CLI.id_cliente = FAC.id_cliente
+INNER JOIN t_producto PRO ON PRO.id_producto = PRO.id_producto
